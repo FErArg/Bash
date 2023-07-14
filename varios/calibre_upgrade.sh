94c884a2bda3126b1a88884be46d1632d3fcfd16
FErArg <ferarg@ferarg.com>
2023-07-14 17:05:34 +0200

    upgrade  comparison line

--- varios/calibre_upgrade.sh
+++ varios/calibre_upgrade.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
 # Get the installed version of Calibre
-installed_version=$(ebook-viewer --version | awk '{ print substr( $3, 1, length($3)-1 ) }')
-  
+calibre=$(which calibre)
+installed_version=$($calibre --version | awk '{ print substr( $3, 1, length($3)-1 ) }')
+
 # Get the latest version of Calibre from the web
 latest_version=$(curl -s https://calibre-ebook.com/whats-new | grep -oP --max-count=1 'Release: \K[\d.]+')
-  
-# Compare the versions
-if [[ $installed_version == $latest_version ]]; then
+
+echo "Installed: $installed_version - Latest: $latest_version"
+
+# Compare the versions ${FOO%.*}
+if [ "$installed_version" = "$latest_version" ];then
+    # TRUE
     echo "You have the latest version of Calibre ($installed_version)."
 else
+    # FALSE
     echo "Your installed version of Calibre is $installed_version, and the latest version is $latest_version."
     sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 fi
